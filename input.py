842d30984d54232a32da19daf13a8191fc9d4b83
chidiebere unegbu <chidiebereunegby@yahoo.com>
2024-01-12 22:06:03 +0100

    worked on user input

--- input.py
+++ input.py
@@ -13,13 +13,16 @@ def days_to_secs():
     days = input("Enter the number of days to calculate the total seconds: \n")
     days_int = int(days)
     days_in_seconds = one_day_in_seconds * days_int
-    if days_int == 1:
-        print(f'{days} day_in_seconds is evaluated as {days_in_seconds}')
+    if days_int <= 0:
+        print("Input can only accept a positive number, kindly enter a positive number")
+    elif days_int == 1:
+        print(f'{days} day is evaluated as {days_in_seconds} seconds')
     else:
-        print(f'{days} days_in_seconds is evaluated as {days_in_seconds}')
+        print(f'{days} days is evaluated as {days_in_seconds} seconds')
 
 # function without arguments
 
 
+days_to_secs()
 days_to_secs()
 days_to_secs()
